803dd78fcbd334b6428fd64263a74d41abfa58c6
Cube <cube@so-icy-ent.biz>
2026-04-04 16:26:10 -0600

    header changes (rough)

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,9 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Alex Williams Blog";
+export const CONSOLE_USER = `alexw`;
+export const CONSOLE_PWD = `~/dev/blog`;
+export const CONSOLE_PROMPT = `?`;
+export const CONSOLE_DATE = `${new Date().toDateString().toLocaleLowerCase()}`;
+export const CONSOLE_TIME = `${new Date().toLocaleTimeString()}`;
 export const SITE_DESCRIPTION = "Welcome to my blog!";
